668ad8894350afbbe8beb5a933e5cb5e083260b7
slontia <654867229@qq.com>
2021-08-13 01:03:40 +0800

    fix format

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,9 @@
-// 注意: 本项目的所有源文件都必须是 UTF-8 编码
-
-// 这是一个“反撤回”机器人
-// 在群里回复 “/anti-recall enabled.” 或者 “撤回没用” 之后
-// 如果有人在群里撤回，那么机器人会把撤回的内容再发出来
-
 #include <iostream>
 #include <map>
 #include <mirai.h>
-#include "myheader.h"
 #include <gflags/gflags.h>
-#include "lgtbot/bot_core/bot_core.h"
-#include "lgtbot/utility/msg_sender.h"
+#include "myheader.h"
+#include "bot_core/bot_core.h"
 
 DEFINE_string(ip, "127.0.0.1", "The IP address");
 DEFINE_int32(port, 8080, "The port");
@@ -27,167 +20,224 @@ DEFINE_string(db_passwd, "", "Password of database");
 
 static Cyan::MiraiBot* g_mirai_bot = nullptr;
 
-class MyMsgSender : public MsgSender
+struct Messager
 {
- public:
-  MyMsgSender(const Target target, const uint64_t id) : target_(target), id_(id) {}
-  virtual ~MyMsgSender() override
-  {
-    try
-    {
-      if (target_ == TO_USER)
-      {
-        g_mirai_bot->SendMessage(Cyan::QQ_t(id_), msg_);
-      }
-      else if (target_ == TO_GROUP)
-      {
-        g_mirai_bot->SendMessage(Cyan::GID_t(id_), msg_);
-      }
-    }
-    catch (std::exception& e)
-    {
-      std::cerr << "Sending message failed: target=" << target_ << " id=" << id_ << " info=" << e.what() << std::endl;
-    }
-  }
-  virtual void SendString(const char* const str, const size_t len) override { msg_.Plain(std::string_view(str, len)); }
-  virtual void SendAt(const uint64_t uid) override
-  {
-    if (target_ == TO_USER)
-    {
-      if (uid == id_) {
-        msg_.Plain("<你>");
-      } else {
-        msg_.Plain("<" + std::to_string(uid) + ">");
-      }
-    }
-    else if (target_ == TO_GROUP)
-    {
-      msg_.At(Cyan::QQ_t(uid));
-    }
-  }
-
- private:
-  const Target target_;
-  const uint64_t id_;
-  Cyan::MessageChain msg_;
+    Messager(const uint64_t id, const bool is_uid) : id_(id), is_uid_(is_uid) {}
+    const uint64_t id_;
+    const bool is_uid_;
+    Cyan::MessageChain msg_;
 };
 
-MsgSender* new_msg_sender(const Target target, const uint64_t id) { return new MyMsgSender(target, id); }
-void delete_msg_sender(MsgSender* msg_sender) { delete msg_sender; }
-
-static const std::vector<UserID> LoadAdmins()
+void* OpenMessager(const uint64_t id, const bool is_uid)
 {
-  if (FLAGS_admins.empty()) { return {}; }
-  std::vector<UserID> admins;
-  std::string::size_type begin = 0;
-  while (true)
-  {
-    admins.emplace_back(atoll(FLAGS_admins.c_str() + begin));
-    begin = FLAGS_admins.find_first_of(',', begin);
-    if (begin == std::string::npos || ++begin == FLAGS_admins.size()) { break; }
-  }
-  return admins;
+    return new Messager(id, is_uid);
+}
+
+void MessagerPostText(void* p, const char* data, uint64_t len)
+{
+    Messager* const messager = static_cast<Messager*>(p);
+    messager->msg_.Plain(std::string_view(data, len));
+}
+
+void MessagerPostUser(void* p, uint64_t uid, bool is_at)
+{
+    Messager* const messager = static_cast<Messager*>(p);
+
+    const auto post_user_id = [&](const uint64_t uid)
+    {
+        messager->msg_.Plain("<" + std::to_string(uid) + ">");
+    };
+
+    const auto try_post_name = [&](const uint64_t uid)
+    {
+        try {
+            for (const auto& f : g_mirai_bot->GetFriendList()) {
+                if (uid == f.QQ.ToInt64()) {
+                    messager->msg_.Plain("<" + f.NickName + "(" + std::to_string(uid) + ")>");
+                    return;
+                }
+            }
+            // If the user is not friend, we will not find his name.
+        } catch (std::exception& e) {
+            std::cerr << "UserName failed: uid=" << uid << " info=" << e.what() << std::endl;
+        }
+        post_user_id(uid);
+    };
+
+    const auto try_post_member_name = [&](const uint64_t uid, const uint64_t gid)
+    {
+        try {
+            messager->msg_.Plain("<" + g_mirai_bot->GetGroupMemberInfo(Cyan::GID_t(gid), Cyan::QQ_t(uid)).MemberName
+                           + "(" + std::to_string(uid) + ")>");
+            return;
+        } catch (std::exception& e) {
+            std::cerr << "GroupUserName failed: uid=" << uid << " gid=" << gid << " info=" << e.what() << std::endl;
+        }
+        try_post_name(uid);
+    };
+
+    if (is_at) {
+        if (!messager->is_uid_) {
+            messager->msg_.At(Cyan::QQ_t(uid));
+        } else if (uid == messager->id_) {
+            messager->msg_.Plain("<你>");
+        } else {
+            try_post_name(uid);
+        }
+    } else {
+        if (!messager->is_uid_) {
+            try_post_member_name(uid, messager->id_);
+        } else {
+            try_post_name(uid);
+        }
+    }
+}
+
+void MessagerFlush(void* p)
+{
+    Messager* const messager = static_cast<Messager*>(p);
+    try {
+        if (messager->is_uid_) {
+            g_mirai_bot->SendMessage(Cyan::QQ_t(messager->id_), messager->msg_);
+        } else {
+            g_mirai_bot->SendMessage(Cyan::GID_t(messager->id_), messager->msg_);
+        }
+    } catch (std::exception& e) {
+        std::cerr << "Sending message failed: is_uid=" << std::boolalpha << messager->is_uid_ << std::noboolalpha
+                  << " id=" << messager->id_ << " info=" << e.what() << std::endl;
+    }
+    messager->msg_.Clear();
+}
+
+void CloseMessager(void* p)
+{
+    Messager* const messager = static_cast<Messager*>(p);
+    delete messager;
+}
+
+static const std::vector<uint64_t> LoadAdmins()
+{
+    if (FLAGS_admins.empty()) {
+        return {};
+    }
+    std::vector<uint64_t> admins;
+    std::string::size_type begin = 0;
+    while (true) {
+        admins.emplace_back(atoll(FLAGS_admins.c_str() + begin));
+        begin = FLAGS_admins.find_first_of(',', begin);
+        if (begin == std::string::npos || ++begin == FLAGS_admins.size()) {
+            break;
+        }
+    }
+    return admins;
 }
 
 static void ConnectDatabase(void* const bot)
 {
-  if (!FLAGS_db_addr.empty())
-  {
-    const char* errmsg = nullptr;
-    if (FLAGS_db_passwd.empty())
-    {
-      char passwd[128] = {0};
-      std::cout << "Password: ";
-      std::cin >> passwd;
-      BOT_API::ConnectDatabase(bot, FLAGS_db_addr.c_str(), FLAGS_db_user.c_str(), passwd, FLAGS_db_name.c_str(), &errmsg);
+#ifdef WITH_MYSQL
+    if (!FLAGS_db_addr.empty()) {
+        const char* errmsg = nullptr;
+        if (FLAGS_db_passwd.empty()) {
+            char passwd[128] = {0};
+            std::cout << "Password: ";
+            std::cin >> passwd;
+            BOT_API::ConnectDatabase(bot, FLAGS_db_addr.c_str(), FLAGS_db_user.c_str(), passwd, FLAGS_db_name.c_str(), &errmsg);
+        } else {
+            BOT_API::ConnectDatabase(bot, FLAGS_db_addr.c_str(), FLAGS_db_user.c_str(), FLAGS_db_passwd.c_str(), FLAGS_db_name.c_str(), &errmsg);
+        }
+        if (errmsg) {
+            std::cerr << "Connect database failed errmsg:" << *errmsg << std::endl;
+        } else {
+            std::cout << "Connect database success" << std::endl;
+        }
     }
-    else
-    {
-      BOT_API::ConnectDatabase(bot, FLAGS_db_addr.c_str(), FLAGS_db_user.c_str(), FLAGS_db_passwd.c_str(), FLAGS_db_name.c_str(), &errmsg);
-    }
-    if (errmsg)
-    {
-      std::cerr << "Connect database failed errmsg:" << *errmsg << std::endl;
-    }
-    else
-    {
-      std::cout << "Connect database success" << std::endl;
-    }
-  }
+#endif
 }
 
 int main(int argc, char** argv)
 {
 #if defined(WIN32) || defined(_WIN32)
-	// 切换代码页，让 CMD 可以显示 UTF-8 字符
+    // make Windows console show UTF-8 characters
 	system("chcp 65001");
 #endif
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
-  const auto admins = LoadAdmins();
-  const std::unique_ptr<void, void(*)(void*)> bot_core(
-      BOT_API::Init(FLAGS_qq, new_msg_sender, delete_msg_sender, FLAGS_game_path.c_str(), admins.data(), admins.size()),
-      BOT_API::Release);
-  if (!bot_core)
-  {
-    std::cerr << "Init bot core failed" << std::endl;
-    return -1;
-  }
-  ConnectDatabase(bot_core.get());
-  std::cout << "[QQ] " << FLAGS_qq << std::endl;
-  std::cout << "[Address] " << FLAGS_ip << ":" << FLAGS_port << std::endl;
-  Cyan::MiraiBot bot(FLAGS_ip, FLAGS_port, FLAGS_thread);
-  g_mirai_bot = &bot;
-	while (true)
-	{
-		try
-		{
-			bot.Auth(FLAGS_auth, Cyan::QQ_t(FLAGS_qq));
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    const auto admins = LoadAdmins();
+    const std::unique_ptr<void, void(*)(void*)> bot_core(
+            BOT_API::Init(FLAGS_qq, FLAGS_game_path.c_str(), admins.data(), admins.size()),
+            BOT_API::Release);
+    if (!bot_core) {
+        std::cerr << "Init bot core failed" << std::endl;
+        return -1;
+    }
+    ConnectDatabase(bot_core.get());
+
+    Cyan::MiraiBot bot;
+    Cyan::SessionOptions opts;
+    opts.BotQQ = Cyan::QQ_t(FLAGS_qq);
+    opts.HttpHostname = FLAGS_ip;
+    opts.WebSocketHostname = FLAGS_ip;
+    opts.HttpPort = FLAGS_port;
+    opts.WebSocketPort = FLAGS_port;
+    opts.VerifyKey = FLAGS_auth;
+    //opts.ThreadPoolSize = FLAGS_thread;
+
+    g_mirai_bot = &bot;
+    while (true) {
+        try {
+            bot.Connect(opts);
+            break;
+        } catch (const std::exception& ex) {
+            std::cerr << "connect bot failed errmsg:" << ex.what() << std::endl;
+        }
+        Cyan::MiraiBot::SleepSeconds(1);
+    }
+    std::cout << "Bot Working..." << std::endl;
+
+    bot.On<Cyan::GroupMessage>([&bot_core](Cyan::GroupMessage m)
+        {
+            try {
+                if (m.AtMe()) {
+                    BOT_API::HandlePublicRequest(bot_core.get(), m.Sender.Group.GID.ToInt64(), m.Sender.QQ.ToInt64(),
+                            m.MessageChain.GetPlainText().c_str());
+                }
+            } catch (const std::exception& ex) {
+                std::cout << ex.what() << std::endl;
+            }
+        });
+
+    bot.On<Cyan::FriendMessage>([&bot_core](Cyan::FriendMessage m)
+        {
+            try {
+                BOT_API::HandlePrivateRequest(bot_core.get(), m.Sender.QQ.ToInt64(), m.MessageChain.GetPlainText().c_str());
+            } catch (const std::exception& ex) {
+                std::cout << ex.what() << std::endl;
+            }
+        });
+
+    bot.On<Cyan::TempMessage>([&bot_core](Cyan::TempMessage m)
+        {
+            try {
+                m.Reply("[错误] 请先添加我为好友吧，这样我们就能一起玩耍啦~" + m.MessageChain);
+            } catch (const std::exception& ex) {
+                std::cout << ex.what() << std::endl;
+            }
+        });
+
+    bot.OnEventReceived<Cyan::NewFriendRequestEvent>([&](Cyan::NewFriendRequestEvent newFriend)
+        {
+            try {
+                newFriend.Accept();
+            } catch (const std::exception& ex) {
+                std::cout << ex.what() << std::endl;
+            }
+        });
+
+	for (std::string command; std::cin >> command; ) {
+		if (command == "exit") {
+			bot.Disconnect();
 			break;
 		}
-		catch (const std::exception& ex)
-		{
-      std::cout << ex.what() << std::endl;
-		}
-    Cyan::MiraiBot::SleepSeconds(1);
 	}
-  std::cout << "Bot Working..." << std::endl;
 
-  bot.On<Cyan::GroupMessage>([&bot_core](Cyan::GroupMessage m)
-    {
-      try
-      {
-        if (m.AtMe()) { BOT_API::HandlePublicRequest(bot_core.get(), m.Sender.Group.GID, m.Sender.QQ, m.MessageChain.GetPlainText().c_str()); }
-      }
-			catch (const std::exception& ex) { std::cout << ex.what() << std::endl; }
-    });
-
-  bot.On<Cyan::FriendMessage>([&bot_core](Cyan::FriendMessage m)
-    {
-      try
-      {
-        BOT_API::HandlePrivateRequest(bot_core.get(), m.Sender.QQ, m.MessageChain.GetPlainText().c_str());
-      }
-			catch (const std::exception& ex) { std::cout << ex.what() << std::endl; }
-    });
-
-  bot.On<Cyan::TempMessage>([&bot_core](Cyan::TempMessage m)
-    {
-      try
-      {
-        BOT_API::HandlePrivateRequest(bot_core.get(), m.Sender.QQ, m.MessageChain.GetPlainText().c_str());
-      }
-			catch (const std::exception& ex) { std::cout << ex.what() << std::endl; }
-    });
-
-	bot.OnEventReceived<Cyan::NewFriendRequestEvent>([&](Cyan::NewFriendRequestEvent newFriend)
-		{
-			newFriend.Accept();
-		});
-
-	bot.EventLoop([](const char* err_msg)
-		{
-      std::cout << "[bot错误] " << err_msg << std::endl;
-		});
-
-	return 0;
+    return 0;
 }
